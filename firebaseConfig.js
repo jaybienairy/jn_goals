aced55a67aef3e009df3cc4aede7f3acbcd17bdb
jaybienairy <jaybienairy@gmail.com>
2025-08-27 09:46:41 +0800

    Firebase Config

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2bnFfkLX2VpZ1FXOFG-5M51HL9fEoaOc",
+  authDomain: "jn-goals.firebaseapp.com",
+  projectId: "jn-goals",
+  storageBucket: "jn-goals.firebasestorage.app",
+  messagingSenderId: "987462477452",
+  appId: "1:987462477452:web:bd47835f829de9820d3da9"
+};
+
+
+const app = initializeApp(firebaseConfig);
